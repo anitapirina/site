5c7991249ce20f47ef289ce402d47727421ee5bd
Anna Spirina <anita-md@mail.ru>
2019-01-31 08:08:33 -0500

    site

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(function () {
+	$(".navbar-toggler").blur(function (event){
+		var scrinwidth = window.innerWidth;
+		if (scrinwidth < 768) {
+			$(".navbar-collapse").collapse('hide');
+		}
+	});
+});
+
+(function (global) {
+	var dc={};
+	var homedata = "snipets/main-content.html";
+
+	var insertHTML = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+	var showLoading = function(selector){
+		var html = "<div class='text-center'></div>";
+		insertHTML (selector, html);
+	}
+
+	document.addEventListener("DOMContentLoaded", function (event){
+		$ajaxUtils.sendGetRequest(
+			homedata, 
+			function (responseText) {
+			document.querySelector(".main-content").innerHTML = responseText;
+		}, 
+			false);
+	});
+	global.$dc = dc;
+
+}) (window);
