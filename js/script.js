c38752bc74dc096fb11d8e16d12c3b3e66cb9cef
Anna Spirina <anita-md@mail.ru>
2019-02-02 14:41:46 -0500

    site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Функция скрывает раскрытое меню при потере фокуса
 $(function () {
 	$(".navbar-toggler").blur(function (event){
 		var scrinwidth = window.innerWidth;
@@ -7,30 +8,36 @@ $(function () {
 	});
 });
 
+
 (function (global) {
+	//Объявление переменных и ссылок на файлы
 	var dc={};
 	var homedata = "snipets/main-content.html";
 	var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
 	var categoriesTitle = "snipets/menu-snipet.html";
 	var categoriesHtml = "snipets/categories-snipet.html";
 
+	//Функция замены содержимого main-content на главной страницы при переходе на другие страницы
 	var insertHTML = function (selector, html) {
 	var targetElem = document.querySelector(selector);
 	targetElem.innerHTML = html;
 	};
 
+	//Добавление анимации загрузки 
 	var showLoading = function(selector){
 	var html = "<div class='text-center'>"
 	html += "<img src ='images/ajax-loader.gif'></div>";
 	insertHTML (selector, html);
 	}
 
+	//Замена содержимого в шаблонах
 	var insertProperty = function (string, propName, propValue) {
 		var propToReplace = "{{" + propName + "}}";
 		string = string.replace (new RegExp(propToReplace, "g"),propValue);
 		return string;
 	}
 
+	//Запрос содержимого из файла main-content для добавления на главную страницу
 	document.addEventListener("DOMContentLoaded", function (event){
 		$ajaxUtils.sendGetRequest(
 			homedata, 
@@ -40,12 +47,14 @@ $(function () {
 			false);
 	});
 
+	//Функция загрузки категорий меню
 	dc.loadMenuCategories = function (){
 		showLoading(".main-content");
 		$ajaxUtils.sendGetRequest(
 			allCategoriesUrl, buildAndShowCategories);
 	};
 
+	//
 	function buildAndShowCategories(categories) {
 		showLoading(".main-content");
 		$ajaxUtils.sendGetRequest(
@@ -57,6 +66,8 @@ $(function () {
 					}, false);
 			}, false);
 	}
+
+	//фукнция конструктор, перебирает все категории в файле json и формирует вид итоговой страницы с классом row
 	function buildCategories(categories, categoriesTitle, categoriesHtml) {
 		var finalHtml = categoriesTitle;
 		finalHtml+="<section class='row'>";
@@ -66,12 +77,15 @@ $(function () {
 			var name = "" + categories[i].name;
 			var short_name = categories[i].short_name;
 			html = insertProperty(html, "name", name);
+			alert(html);
 			html = insertProperty(html, "short_name", short_name);
+			alert(html);
 			finalHtml+=html;
 		}
 		finalHtml+="</section>";
 		return finalHtml;
 	}
+	//конец функции загрузки категорий меню
 	
 global.$dc = dc;
 })(window);
