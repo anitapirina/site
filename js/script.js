8e71e49e30a3979213956b61e842d53d0d1080ca
Anna Spirina <anita-md@mail.ru>
2019-02-03 15:22:04 -0500

    site

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ $(function () {
 	var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
 	var categoriesTitle = "snipets/menu-snipet.html";
 	var categoriesHtml = "snipets/categories-snipet.html";
+	var menuItemUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?categories=";
+	var menuItemTitle = "snipets/menu-item-title.html";
+	var menuItemHtml = "snipets/menu-item.html";
 
 	//Функция замены содержимого main-content на главной страницы при переходе на другие страницы
 	var insertHTML = function (selector, html) {
@@ -54,6 +57,13 @@ $(function () {
 			allCategoriesUrl, buildAndShowCategories);
 	};
 
+	//Загрузка блюд в выбранной категорииб shortNameCategort - короткое имя выбранной категории
+	dc.loadMenuItems = function(shortNameCategory){
+		showLoading(".main-content");
+		$ajaxUtils.sendGetRequest(
+			menuItemUrl + shortNameCategory, buildAndShowMenuItems);
+	};
+
 	//
 	function buildAndShowCategories(categories) {
 		showLoading(".main-content");
@@ -83,7 +93,68 @@ $(function () {
 		finalHtml+="</section>";
 		return finalHtml;
 	}
-	//конец функции загрузки категорий меню
+	
+	function buildAndShowMenuItems(menuItems) {
+		showLoading(".main-content");
+		$ajaxUtils.sendGetRequest (
+			menuItemTitle, function(menuItemTitle){
+				$ajaxUtils.sendGetRequest (
+					menuItemHtml, function(menuItemHtml){
+						var itemsView = buitdItemsView(menuItems, menuItemTitle, menuItemHtml);
+						insertHTML(".main-content", itemsView);
+					}, false);
+			}, false);
+	}
+
+	function buitdItemsView (menuItems, menuItemTitle, menuItemHtml) {
+		menuItemTitle=insertProperty(menuItemTitle, "name", menuItems.category.name);
+		menuItemTitle=insertProperty(menuItemTitle, "special_instructions", menuItems.category.special_instructions);
+
+		var finalHtml=menuItemTitle;
+		finalHtml+="<section class='row'>";
+
+		var Items = menuItems.menu_items;
+		var catShortName = menuItems.category.short_name;
+
+		for (var i = 0; i<menuItems.length; i++) {
+			var html = menuItemHtml;
+			html = insertProperty(html, "short_name", menuItems[i].short_name);
+			html = insertProperty(html, "catShortName", catShortName);
+			html = insertPrice(html, "price_small", menuItems[i].price_small);
+			html = insertPortionName(html, "small_portion_name", menuItems[i].small_portion_name);
+			html = insertPrice(html, "price_large", menuItems[i].price_large);
+			html = insertPortionName(html, "large_portion_name", menuItems[i].large_portion_name);
+			html = insertProperty(html, "name", name);
+			html = insertProperty(html, "description", description);
+
+			finalHtml+=html;
+		}
+		finalHtml+="</section>";
+		return finalHtml;
+	}
+
+		function insertPrice(html, pricePropName, priceValue){
+			if (!propValue){
+				return insertProperty(html, pricePropName, "");
+			}
+			priceValue="$"+priceValue.toFixed(2);
+			html=insertProperty(html, pricePropName, priceValue);
+			return html;
+		}	
+	function insertPortionName(html,
+                               portionPropName,
+                               portionValue) {
+  // If not specified, return original string
+  if (!portionValue) {
+    return insertProperty(html, portionPropName, "");
+  }
+
+  portionValue = "(" + portionValue + ")";
+  html = insertProperty(html, portionPropName, portionValue);
+  return html;
+}
+
+
 	
 global.$dc = dc;
 })(window);
